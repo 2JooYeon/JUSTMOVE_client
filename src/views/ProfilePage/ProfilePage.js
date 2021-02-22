e511fe3835607345cdbc05aefc3db861ad523431
JongwonYoun <jeffyoun@naver.com>
2021-02-23 01:16:52 +0900

    feat: 프로필 배지 위치 수정

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -3,24 +3,22 @@ import React from "react";
 import classNames from "classnames";
 // @material-ui/core components
 // import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 // @material-ui/icons
 import EmojiEvents from "@material-ui/icons/EmojiEvents";
+import Palette from "@material-ui/icons/Palette";
 // core components
 import Header from "components/Header/Header.js";
 import Footer from "components/Footer/Footer.js";
 import Button from "components/CustomButtons/Button.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
-import Tabs from "components/CustomTabs/CustomTabs.js";
 import NavPills from "components/NavPills/NavPills.js";
 import Parallax from "components/Parallax/Parallax.js";
 
 import profile from "assets/img/faces/christian.jpg";
 import studio1 from "assets/img/examples/studio-1.jpg";
 import studio2 from "assets/img/examples/studio-2.jpg";
-import studio3 from "assets/img/examples/studio-3.jpg";
 import studio4 from "assets/img/examples/studio-4.jpg";
 import studio5 from "assets/img/examples/studio-5.jpg";
 import work1 from "assets/img/examples/olu-eletu.jpg";
@@ -30,148 +28,161 @@ import work4 from "assets/img/examples/mariya-georgieva.jpg";
 import work5 from "assets/img/examples/clem-onojegaw.jpg";
 
 import styles from "assets/jss/material-kit-react/views/profilePage.js";
-import { Icon } from "@material-ui/core";
 
 const useStyles = makeStyles(styles);
 
 export default function ProfilePage(props) {
-  const classes = useStyles();
-  const { ...rest } = props;
-  const imageClasses = classNames(
-    classes.imgRaised,
-    classes.imgRoundedCircle,
-    classes.imgFluid
-  );
-  const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
-  return (
-    <div>
-      <Header
-        color="transparent"
-        brand="JUSTMOVE"
-        rightLinks=""
-        user="클러그 님"
-        fixed
-        changeColorOnScroll={{
-          height: 200,
-          color: "white"
-        }}
-        {...rest}
-      />
-      <Parallax small filter image={require("assets/img/profile-bg.jpg")} />
-      <div className={classNames(classes.main, classes.mainRaised)}>
+    const classes = useStyles();
+    const {...rest} = props;
+    const imageClasses = classNames(
+        classes.imgRaised,
+        classes.imgRoundedCircle,
+        classes.imgFluid
+    );
+    const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
+    return (
         <div>
-          <div className={classes.container}>
-            <GridContainer justify="center" align= "center">
-              <GridItem xs={12} sm={12} md={6}>
-                <div className={classes.profile}>
-                  <div>
-                    <img src={profile} alt="..." className={imageClasses} />
-                  </div>
-                  <div className={classes.name}>
-                    <h3 className={classes.title}>Christian Louboutin</h3>
-                  </div>
+            <Header
+                color="transparent"
+                brand="JUSTMOVE"
+                rightLinks=""
+                user="클러그 님"
+                fixed
+                changeColorOnScroll={{
+                    height: 200,
+                    color: "white"
+                }}
+                {...rest}
+            />
+            <Parallax small filter image={require("assets/img/profile-bg.jpg")}/>
+            <div className={classNames(classes.main, classes.mainRaised)}>
+                <div>
+                    <div className={classes.container}>
+                        <GridContainer justify="center" align="center">
+                            <GridItem xs={12} sm={12} md={6}>
+                                <div className={classes.profile}>
+                                    <div>
+                                        <img src={profile} alt="..." className={imageClasses}/>
+                                    </div>
+                                    <div className={classes.name}>
+                                        <h3 className={classes.title}>Christian Louboutin</h3>
+                                    </div>
+                                </div>
+                                <div className={classes.description}>
+                                    <p>
+                                        가만히 못 앉아있는 사람{" "}
+                                    </p>
+                                </div>
+                            </GridItem>
+                            <GridItem xs={12} sm={12} md={6} style={{display:"flex", alignItems: "center"}}>
+
+                                <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                    <div style={{margin:"0 30px"}}>
+                                        <EmojiEvents size="large"/>
+                                        <h6 style={{fontSize: "1.1rem", fontWeight: "normal"}}>BADGE</h6>
+                                    </div>
+                                    <div style={{margin:"0 30px", height: "55px", display:"flex", flexDirection:"column"}}>
+                                        <b style={{fontSize: "1.5rem", marginBottom: "7.5px"}}>
+                                            30
+                                        </b>
+                                        <span>
+                                            Follower
+                                        </span>
+                                    </div>
+                                    <div style={{margin:"0 30px", height: "55px", display:"flex", flexDirection:"column"}}>
+                                        <b style={{fontSize: "1.5rem", marginBottom: "7.5px"}}>
+                                            30
+                                        </b>
+                                        <span>
+                                            Following
+                                        </span>
+                                    </div>
+                                </div>
+                            </GridItem>
+
+                        </GridContainer>
+                        <GridContainer justify="center">
+                            <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
+                                <NavPills
+                                    alignCenter
+                                    color="primary"
+                                    tabs={[
+                                        {
+                                            tabButton: "내가 업로드한 영상",
+                                            tabContent: (
+                                                <GridContainer justify="center">
+                                                    <GridItem xs={12} sm={12} md={4}>
+                                                        <img
+                                                            alt="..."
+                                                            src={studio1}
+                                                            className={navImageClasses}
+                                                        />
+                                                        <img
+                                                            alt="..."
+                                                            src={studio2}
+                                                            className={navImageClasses}
+                                                        />
+                                                    </GridItem>
+                                                    <GridItem xs={12} sm={12} md={4}>
+                                                        <img
+                                                            alt="..."
+                                                            src={studio5}
+                                                            className={navImageClasses}
+                                                        />
+                                                        <img
+                                                            alt="..."
+                                                            src={studio4}
+                                                            className={navImageClasses}
+                                                        />
+                                                    </GridItem>
+                                                </GridContainer>
+                                            )
+                                        },
+                                        {
+                                            tabButton: "내가 도전한 영상",
+                                            tabIcon: Palette,
+                                            tabContent: (
+                                                <GridContainer justify="center">
+                                                    <GridItem xs={12} sm={12} md={4}>
+                                                        <img
+                                                            alt="..."
+                                                            src={work1}
+                                                            className={navImageClasses}
+                                                        />
+                                                        <img
+                                                            alt="..."
+                                                            src={work2}
+                                                            className={navImageClasses}
+                                                        />
+                                                        <img
+                                                            alt="..."
+                                                            src={work3}
+                                                            className={navImageClasses}
+                                                        />
+                                                    </GridItem>
+                                                    <GridItem xs={12} sm={12} md={4}>
+                                                        <img
+                                                            alt="..."
+                                                            src={work4}
+                                                            className={navImageClasses}
+                                                        />
+                                                        <img
+                                                            alt="..."
+                                                            src={work5}
+                                                            className={navImageClasses}
+                                                        />
+                                                    </GridItem>
+                                                </GridContainer>
+                                            )
+                                        }
+                                    ]}
+                                />
+                            </GridItem>
+                        </GridContainer>
+                    </div>
                 </div>
-            <div className={classes.description}>
-              <p>
-                가만히 못 앉아있는 사람{" "}
-              </p>
+                <Footer/>
             </div>
-            </GridItem>
-            <GridItem xs={12} sm={12} md={6}>
-            
-              <div vertical-align="middle">
-            <div className={classes.wrapper}>
-              <div>
-              <EmojiEvents size="large"/>
-                <h6>BADGE</h6>
-             </div>
-              <Button>30</Button>
-              <Button>30</Button>
-            </div>
-            </div>
-            </GridItem>
-            
-            </GridContainer>
-            <GridContainer justify="center">
-              <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
-                <NavPills
-                  alignCenter
-                  color="primary"
-                  tabs={[
-                    {
-                      tabButton: "내가 업로드한 영상",
-                      tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={studio1}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={studio2}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={studio5}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={studio4}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                        </GridContainer>
-                      )
-                    },
-                    {
-                      tabButton: "내가 도전한 영상",
-                      tabIcon: Palette,
-                      tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={work1}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={work2}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={work3}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={work4}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={work5}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                        </GridContainer>
-                      )
-                    }
-              </GridItem>
-            </GridContainer>
         </div>
-      </div>
-      <Footer />
-    </div>
-  </div>
-  );
+    );
 }
