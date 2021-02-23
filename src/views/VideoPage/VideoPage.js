bfd7424f75436efb6d0a4d24522e3edfbbf8011f
JongwonYoun <jeffyoun@naver.com>
2021-02-23 23:46:57 +0900

    [fix]: VideoPage css 수정

--- src/views/VideoPage/VideoPage.js
+++ src/views/VideoPage/VideoPage.js
@@ -16,8 +16,6 @@ import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import PausePresentationIcon from '@material-ui/icons/PausePresentation';
 import Close from "@material-ui/icons/Close";
 // core components
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
 import Button from "components/CustomButtons/Button.js";
 
 import styles from "assets/jss/material-kit-react/views/videoPage.js";
@@ -25,15 +23,17 @@ import styles from "assets/jss/material-kit-react/views/videoPage.js";
 import image from "assets/img/stage2.jpg";
 import profile from "assets/img/faces/christian.jpg";
 
+import video from "assets/video/Alone.mp4"
+
 import videoSrc from "assets/video/example.mp4";
 
 const useStyles = makeStyles(styles);
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="down" ref={ref} {...props} />;
-  });
-  
-  Transition.displayName = "Transition";
+});
+
+Transition.displayName = "Transition";
 
 export default function ProfilePage(props) {
     const classes = useStyles();
@@ -46,115 +46,119 @@ export default function ProfilePage(props) {
     const [classicModal, setClassicModal] = React.useState(false);
     return (
         <div
-        className={classes.pageHeader}
-        style={{
-          backgroundImage: "url(" + image + ")",
-          backgroundSize: "cover",
-          backgroundPosition: "top center",
-          display: "flex"
-        }}
+            className={classes.pageHeader}
+            style={{
+                backgroundImage: "url(" + image + ")",
+                backgroundSize: "cover",
+                backgroundPosition: "top center",
+                display: "flex"
+            }}
         >
-            <div className={classes.container}>
-                <div align="right">
-                    <PausePresentationIcon style={{ fontSize: 80 }}/>
-                    <GridItem xs={12} sm={12} md={6} lg={4}>
-                <Button
-                  color="primary"
-                  block
-                  onClick={() => setClassicModal(true)}
-                >
-                  <ExitToAppIcon style={{ fontSize: 80 }}/>
-                </Button>
-                <Dialog
-                  classes={{
-                    root: classes.center,
-                    paper: classes.modal
-                  }}
-                  open={classicModal}
-                  TransitionComponent={Transition}
-                  keepMounted
-                  onClose={() => setClassicModal(false)}
-                  aria-labelledby="classic-modal-slide-title"
-                  aria-describedby="classic-modal-slide-description"
-                >
-                  <DialogTitle
-                    id="classic-modal-slide-title"
-                    disableTypography
-                    className={classes.modalHeader}
-                  >
-                    <IconButton
-                      className={classes.modalCloseButton}
-                      key="close"
-                      aria-label="Close"
-                      color="inherit"
-                      onClick={() => setClassicModal(false)}
+
+            <div style={{position: "relative", width: "100%", height: "100vh"}}>
+                <div style={{display: "flex", position: "absolute", top: "0px", right: "10px"}}>
+                    <PausePresentationIcon style={{fontSize: 80}}/>
+                    <button
+                        style={{border: "none", outline: "none", background: "transparent", marginLeft: "5px"}}
+                        onClick={() => setClassicModal(true)}
                     >
-                      <Close className={classes.modalClose} />
-                    </IconButton>
-                    <h4 className={classes.modalTitle}>지금 종료하시겠어요?</h4>
-                  </DialogTitle>
-                  <DialogContent
-                    id="classic-modal-slide-description"
-                    className={classes.modalBody}
-                  >
-                    <p>
-                      현재까지 누적하신 점수가 초기화 됩니다.
-                    </p>
-                  </DialogContent>
-                  <DialogActions className={classes.modalFooter}>
-                    <Button 
-                    color="transparent" 
-                    simple
-                    onClick={() => setClassicModal(false)}
-                    simple
-                    >
-                      이어하기
-                    </Button>
-                    <Button
-                    color="danger"
-                    >
-                      종료
-                    </Button>
-                  </DialogActions>
-                </Dialog>
-              </GridItem>
+                        <ExitToAppIcon style={{fontSize: 80}}/>
+                    </button>
                 </div>
-                <GridContainer justify="center" align="center">
-                    <GridItem xs={6} sm={6} md={6}>
-                        <div className={classes.video} style={{backgroundColor: "#FFFFFF"}}>
-                            {/* <video
-                            loop={false}
-                            >
-                                <source src={videoSrc}></source>
-                            </video> */}
-                        <div className={classes.profile}>
-                            <div>
-                                <img src={profile} alt="..." className={imageClasses}/>
+                <div style={{display: "flex", height: "100%"}}>
+                    <div style={{
+                        flex: "1",
+                        position: "relative",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}>
+                        <div style={{
+                            position: "absolute",
+                            left: "15px",
+                            top: "140px",
+                            display: "flex",
+                            flexDirection: "column"
+                        }}>
+                            <div className={classes.profile} style={{marginBottom: "15px"}}>
+                                <img src={profile} alt="..." className={imageClasses}
+                                     style={{width: "75px", height: "75px"}}/>
                             </div>
                             <div className={classes.name}>
-                                <h3 className={classes.title} style={{display:"inline"}}>클러그 가자</h3>
+                                <h3 className={classes.title} style={{color: "white"}}>CLUG</h3>
                             </div>
                         </div>
-                        </div>
-                    </GridItem>
-                    <GridItem xs={6} sm={6} md={6}>
-                        <div className={classes.video} style={{backgroundColor: "#FFFFFF"}}>
-                            {/* <video
-                            loop={false}
-                            >
-                                <source src={videoSrc}></source>
-                            </video> */}
-                        <div className={classes.profile}>
-                            <div>
-                                <img src={profile} alt="..." className={imageClasses}/>
+                        <video width="640" height="480" controls src={video}/>
+                    </div>
+                    <div style={{flex: "1", position: "relative", display: "flex", alignItems: "center"}}>
+                        <div style={{
+                            position: "absolute",
+                            right: "15px",
+                            top: "140px",
+                            display: "flex",
+                            flexDirection: "column"
+                        }}>
+                            <div className={classes.profile} style={{marginBottom: "15px"}}>
+                                <img src={profile} alt="..." className={imageClasses}
+                                     style={{width: "75px", height: "75px"}}/>
                             </div>
                             <div className={classes.name}>
-                                <h3 className={classes.title} style={{display:"inline"}}>클러그 가자</h3>
+                                <h3 className={classes.title} style={{color: "white"}}>SOMEONE</h3>
                             </div>
                         </div>
-                        </div>
-                    </GridItem>
-                </GridContainer>
+                        <video width="640" height="480" controls src={videoSrc}/>
+                    </div>
+                </div>
+                <Dialog
+                    classes={{
+                        root: classes.center,
+                        paper: classes.modal
+                    }}
+                    open={classicModal}
+                    TransitionComponent={Transition}
+                    keepMounted
+                    onClose={() => setClassicModal(false)}
+                    aria-labelledby="classic-modal-slide-title"
+                    aria-describedby="classic-modal-slide-description"
+                >
+                    <DialogTitle
+                        id="classic-modal-slide-title"
+                        disableTypography
+                        className={classes.modalHeader}
+                    >
+                        <IconButton
+                            className={classes.modalCloseButton}
+                            key="close"
+                            aria-label="Close"
+                            color="inherit"
+                            onClick={() => setClassicModal(false)}
+                        >
+                            <Close className={classes.modalClose}/>
+                        </IconButton>
+                        <h4 className={classes.modalTitle}>지금 종료하시겠어요?</h4>
+                    </DialogTitle>
+                    <DialogContent
+                        id="classic-modal-slide-description"
+                        className={classes.modalBody}
+                    >
+                        <p>
+                            현재까지 누적하신 점수가 초기화 됩니다.
+                        </p>
+                    </DialogContent>
+                    <DialogActions className={classes.modalFooter}>
+                        <Button
+                            color="transparent"
+                            simple
+                            onClick={() => setClassicModal(false)}
+                            simple
+                        >
+                            이어하기
+                        </Button>
+                        <Button color="danger">
+                            종료
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         </div>
     )
